69dc15c656d4a6916023e6a3e7922320f1a15986
ovovsyankina <olgaovsyankina.ovsyankina@yandex.ru>
2023-02-25 22:15:27 +0300

    init repositories

---
+++ src/components/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./HomePage.module.scss";
+
+const HomePage = () => {
+  return (
+    <div className={styles.root}>
+      <h2>Hello</h2>
+    </div>
+  );
+};
+export default HomePage;
