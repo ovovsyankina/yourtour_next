7335e70e4e4608fd588c4202d7104af2d7cd62ad
ovovsyankina <olgaovsyankina.ovsyankina@yandex.ru>
2023-03-07 20:55:56 +0300

    deploy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
-module.exports = {
-  assetPrefix: isProd ? "/your-github-repo-name/" : "",
-  images: {
-    unoptimized: true,
-  },
+const nextConfig = {
+  reactStrictMode: true,
 };
+
+module.exports = nextConfig;
